ffccccc47335977edbd1fd733ee2613634145d3c
Alex <alex.wang@gofreight.com>
2024-03-22 15:10:13 +0800

    add router

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,16 +12,10 @@ export class HeroesComponent implements OnInit {
   constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   heroes: Hero[] = [];
-  selectedHero?: Hero;
   
   getHeroes(): void{
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes)
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   ngOnInit(): void {
